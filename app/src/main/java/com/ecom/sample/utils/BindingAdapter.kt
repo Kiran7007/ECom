db46c572e2fa29f500713603de55b5e7b5ce85e7
Kiran70007 <karan.punwatkar@gmail.com>
2021-05-01 15:43:38 +0530

    Fixed screen 1 issue

--- app/src/main/java/com/ecom/sample/utils/BindingAdapter.kt
+++ app/src/main/java/com/ecom/sample/utils/BindingAdapter.kt
@@ -2,10 +2,17 @@ package com.ecom.sample.utils
 
 import android.widget.EditText
 import androidx.databinding.BindingAdapter
+import com.ecom.sample.R
 
 @BindingAdapter("android:bindText")
 fun bindEditText(editText: EditText, value: CharSequence) {
     if (editText.text.toString() != value.toString()) {
+        if (value.isNotEmpty()) {
+            editText.setBackgroundResource(R.drawable.entered_quantity_bg)
+        } else {
+            editText.setBackgroundResource(R.drawable.quantity_bg)
+        }
+
         editText.setText(value)
         editText.setSelection(value.length)
     }
